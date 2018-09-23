4d010c5ac5b4cecf9f0489eef45fbe49bcec6c63
Richard B <C:\Users\dmitr\AppData\Roaming\The Bat!>
2018-09-23 17:29:03 +0300

    firstTry

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  let array = equation.split(' ');// your implementation
+  let a = array[0];
+  let b = parseInt(array[3].toString() + array[4].toString());
+  let c = parseInt(array[7].toString() + array[8].toString());
+
+  let x1 = (-1 * b + Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
+  let x2 = (-1 * b - Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
+
+  return [Math.round(x1), Math.round(x2)].sort((a, b) => a - b);
 }
